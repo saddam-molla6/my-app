da842b2678e1b9f16fccb65a5c611e8856a76006
saddam-molla6 <saddammolla6@gmail.com>
2020-04-09 11:33:39 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var p={
+ Name: "Abbas Ali",
+ Job:"On line markating",
+ village:" choto Bahirbag",
+ thana : "Kashiani , Gopal jong"
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit done my first  <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <p>my first react</p> 
+  <h2>{p.Job+" "+ p.Name+" "+p.village+" "+p.thana }</h2>
       </header>
     </div>
   );
